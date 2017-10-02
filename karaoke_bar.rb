fd9198d673e2e3d8f47db403eb01db7b49cdb2c1
Scott Murray <rai.five@gmail.com>
2017-10-02 09:51:07 +0100

    Fourth Commit - Progress

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -6,52 +6,54 @@ class KaraokeBar
   end
 
 
-    def bootup()
+  def bootup()
 
+    puts ""
+    puts "Karaoke Bar v.2000 Software"
+    puts ""
+    puts "What would you like to do? (Enter 1, 2, 3 or 4)"
+    puts ""
+    puts "1 - Create a new room"
+    puts "2 - Add a guest to an existing room"
+    puts "3 - Add a playlist to an existing room"
+    puts "4 - Check all rooms status"
+
+    # Put above into a UI / Text function to refactor to one line.
+
+    choice = gets.chomp
+
+    # Case statements
+    # if choice 1, puts = please enter room name! Great! Name added.  Please now add a guest to your room.  Ok, now just add a playlist from the list below: (Hint, enter the number of the playlist below to see the full song list)
+    case choice
+    when 1
+      puts "Please enter the room name: "
+      room_name = gets.chomp
+      new_room = Room.new(room_name)
+      @rooms << new_room
+      puts "Thanks! Please now add a guest: "
+      guest_name = gets.chomp
+      puts "...and their favourite song, i.e. Africa by Toto: "
+      favourite_song = gets.chomp
+      new_guest = Guest.new(guest_name, favourite_song)
+      puts "Great! #{guest_name} has been added to the #{new_room} room! Have fun!"
+
+    when 2
+      puts "Please enter the guest's name: "
+      guest_name = gets.chomp
+      puts "...and their favourite song, i.e. Africa by Toto: "
+      favourite_song = gets.chomp
+      new_guest = Guest.new(guest_name, favourite_song)
+      puts "Which room would you like to add #{guest_name} to?"
       puts ""
-      puts "Karaoke Bar v.2000 Software"
-      puts ""
-      puts "What would you like to do? (Enter 1, 2, 3 or 4)"
-      puts ""
-      puts "1 - Create a new room"
-      puts "2 - Add a guest to an existing room"
-      puts "3 - Add a playlist to an existing room"
-      puts "4 - Check all rooms status"
+      puts @rooms.to_s
 
-      # Put above into a UI / Text function to refactor to one line.
+      puts "Great! #{guest_name} has been added to the #{new_room} room! Have fun!"
 
-      choice = gets.chomp
 
-      # Case statements
-      # if choice 1, puts = please enter room name! Great! Name added.  Please now add a guest to your room.  Ok, now just add a playlist from the list below: (Hint, enter the number of the playlist below to see the full song list)
-      case choice
-      when 1
-        puts "Please enter the room name: "
-        room_name = gets.chomp
-        new_room = Room.new(room_name)
-        @rooms << new_room
-        puts "Thanks! Please now add a guest: "
-        guest_name = gets.chomp
-        puts "...and their favourite song, i.e. Africa by Toto: "
-        favourite_song = gets.chomp
-        new_guest = Guest.new(guest_name, favourite_song)
-        puts "Great! #{guest_name} has been added to the #{new_room} room! Have fun!"
-
-      when 2
-        puts "Please enter the guest's name: "
-        guest_name = gets.chomp
-        puts "...and their favourite song, i.e. Africa by Toto: "
-        favourite_song = gets.chomp
-        new_guest = Guest.new(guest_name, favourite_song)
-        puts "Which room would you like to add #{guest_name} to?"
-        puts ""
-        puts @rooms.to_s
-
-        puts "Great! #{guest_name} has been added to the #{new_room} room! Have fun!"
-
-      end
 
     end
+
+  end
 
 
 end
---
+++ specs/karaoke_bar_spec.rb
@@ -0,0 +1,25 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('../karaoke_bar')
+
+class TestKaraokeBar < MiniTest::Test
+
+def setup()
+  @karaoke_bar = KaraokeBar.new()
+end
+
+def test_choice_one()
+  
+end
+
+def test_choice_two()
+
+end
+
+def test_choice_three()
+end
+
+def test_choice_four()
+
+end
